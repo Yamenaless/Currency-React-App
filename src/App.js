63297a92570f37a18b6a48b20bd61e5728657162
yamenaless <yamenaless@gmail.com>
2023-07-24 22:45:32 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import Header from "./components/Header";
-import HeroSection from "./components/HeroSection";
-import TestimonialSection from "./components/TestimonialSection";
-import ImageSlider from "./components/ImageSlider";
 
 
 function App() {
   return (
     <div>
-      
     </div>
   );
 }
